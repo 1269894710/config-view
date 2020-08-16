34e366b561c836e4478a224a99326cac83aafe30
1269894710 <1269894710@qq.com>
2020-08-17 00:08:56 +0800

    测试

--- src/commponents/ConfigFlex/ConfigFlex.js
+++ src/commponents/ConfigFlex/ConfigFlex.js
@@ -3,23 +3,23 @@ import './ConfigFlex.less'
 import { flexData } from '../../data.js'
 
 function ConfigFlex() {
-  let [test, setTest] = useState(0)
   let temp = []
   for (let i = 0; i < flexData.row; i++) {
     let row = flexData.list[i]
     let colTemp = []
     for (let n = 0; n < row.col; n++) {
-      colTemp.push(<div className="row-item" style={{"margin-right": row.span}}>
-
+      let item = row.items[n]
+      colTemp.push(<div className="row-item" style={{"margin-right": row.span, "flex": item.flex ? item.flex : 1}}>
+        {item.type}
       </div>)
     }
-    temp.push(<div className="flex-row" style={{"margin": flexData.span}}>{
+    temp.push(<div className="flex-row" style={{"margin-bottom": flexData.span}}>{
       colTemp
     }</div>)
   }
 
   return (
-    <div className="config-flex">
+    <div className="config-flex" id="config-flex" style={{"padding": flexData.span}}>
       { temp }
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -4,15 +4,56 @@ export const flexData = {
   list: [
     {
       col: 3,
-      span: 8
+      span: 8,
+      items: [
+        {
+          type: 'line'
+        },
+        {
+          flex: 1,
+          type: 'bar'
+        },
+        {
+          flex: 1,
+          type: 'pie'
+        }
+      ]
     },
     {
       col: 2,
-      span: 16
+      span: 16,
+      items: [
+        {
+          flex: 3,
+          type: 'line'
+        },
+        {
+          flex: 4,
+          type: 'bar'
+        }
+      ]
     },
     {
       col: 4,
-      span: 10
+      span: 10,
+      items: [
+        {
+          flex: 6,
+          type: 'line'
+        },
+        {
+          flex: 6,
+          type: 'bar'
+        },
+        {
+          flex: 6,
+          type: 'bar'
+        },
+        {
+          flex: 6,
+          type: 'bar'
+        }
+      ]
     }
   ]
 }
